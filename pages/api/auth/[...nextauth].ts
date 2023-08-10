3ed9254572c3a149cb9a4d54ef958648f927b9fc
garethwilliams90 <wgareth90@gmail.com>
2023-08-10 20:18:59 +0300

    github signin

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,48 +19,48 @@ export const authOptions: NextAuthOptions = {
     }),
     //Add another provider...
     GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
-    CredentialsProvider({
-      name: "Sign In",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "example@gmail.com",
-        },
-        name: {
-          label: "Name",
-          type: "name",
-          placeholder: "Michael Tychal",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-        },
-      },
-      async authorize(credentials, req) {
-        if (!credentials || !credentials.email || !credentials.password) {
-          return null
-        }
+    // CredentialsProvider({
+    //   // The name to display on the sign in form (e.g. "Sign in with...")
+    //   name: "Credentials",
+    //   // `credentials` is used to generate a form on the sign in page.
+    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   // You can pass any HTML attribute to the <input> tag through the object.
+    //   credentials: {
+    //     name: {
+    //       label: "Name",
+    //       type: "text",
+    //       placeholder: "Postman Pat",
+    //     },
+    //     email: {
+    //       label: "Email",
+    //       type: "email",
+    //       placeholder: "example@gmail.com",
+    //     },
+    //   },
 
-        const dbUser = await prisma.user.findFirst({
-          where: {
-            email: credentials.email,
-            name: credentials.name,
-          },
-        })
+    //   async authorize(credentials, req) {
+    //     // Add logic here to look up the user from the credentials supplied
+    //     const user = {
+    //       id: credentials?.name,
+    //       email: credentials?.email,
+    //       createdDate: Date.now(),
+    //     }
 
-        //   if (dbUser dbUser.password === credentials.password) {
-        //     const {password, createdAt, id, ...dbWithoutPassword } = dbUser
-        //     return dbUser  as User
-        //   }
+    //     if (user) {
+    //       // Any object returned will be saved in `user` property of the JWT
+    //       return user
+    //     } else {
+    //       // If you return null then an error will be displayed advising the user to check their details.
+    //       return null
 
-        //   return null
-        // },
-      },
-    }),
+    //       // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+    //     }
+    //   },
+    // }),
   ],
   events: {
     createUser: async ({ user }) => {
@@ -89,4 +89,7 @@ export const authOptions: NextAuthOptions = {
   },
 }
 
-export default NextAuth(authOptions)
+const authHandler = NextAuth(authOptions)
+export default async function handler(...params: any[]) {
+  await authHandler(...params)
+}
