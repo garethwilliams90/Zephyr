47f2cbdcb836b39e481b8ab187798833e3e93313
garethwilliams90 <wgareth90@gmail.com>
2023-08-10 20:33:28 +0300

    credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,45 +22,45 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // CredentialsProvider({
-    //   // The name to display on the sign in form (e.g. "Sign in with...")
-    //   name: "Credentials",
-    //   // `credentials` is used to generate a form on the sign in page.
-    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    //   // e.g. domain, username, password, 2FA token, etc.
-    //   // You can pass any HTML attribute to the <input> tag through the object.
-    //   credentials: {
-    //     name: {
-    //       label: "Name",
-    //       type: "text",
-    //       placeholder: "Postman Pat",
-    //     },
-    //     email: {
-    //       label: "Email",
-    //       type: "email",
-    //       placeholder: "example@gmail.com",
-    //     },
-    //   },
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Postman Pat",
+        },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@gmail.com",
+        },
+      },
 
-    //   async authorize(credentials, req) {
-    //     // Add logic here to look up the user from the credentials supplied
-    //     const user = {
-    //       id: credentials?.name,
-    //       email: credentials?.email,
-    //       createdDate: Date.now(),
-    //     }
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = {
+          id: credentials?.name,
+          email: credentials?.email,
+          createdDate: Date.now(),
+        }
 
-    //     if (user) {
-    //       // Any object returned will be saved in `user` property of the JWT
-    //       return user
-    //     } else {
-    //       // If you return null then an error will be displayed advising the user to check their details.
-    //       return null
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
 
-    //       // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-    //     }
-    //   },
-    // }),
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
   ],
   events: {
     createUser: async ({ user }) => {
